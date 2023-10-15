40a96957263f22f78ccc675d7c468942939eacb8
JesseCh3n <105155682+JesseCh3n@users.noreply.github.com>
2023-10-15 21:20:57 +1100

    First commit

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -9,7 +9,9 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -20,6 +22,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -72,7 +75,10 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      // const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { bookData: { ...bookToSave } },
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
@@ -82,6 +88,7 @@ const SearchBooks = () => {
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
+      console.error(error);
     }
   };
 
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,60 @@
+const { User, Book } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id })
+        .select('-__v -password');
+        // returns everything BUT password
+
+      }
+      throw AuthenticationError;
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (_parent, { userId, bookData }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: userId },
+          { $addToSet: { savedBooks: { book: bookData } } },
+          { new: true, runValidators: true }
+        );
+      }
+      throw new AuthenticationError("Please login");
+    },
+    removeBook: async (_parent, { bookId }, context) => {
+      return User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { saveBookss: { _id: bookId } } },
+        { new: true }
+      );
+    },
+  },
+};
+
+module.exports = resolvers;
