724c3026b7cb0d3719fdb3bba8a7e4d2667dbce4
JesseCh3n <105155682+JesseCh3n@users.noreply.github.com>
2023-10-18 03:24:13 +1100

    fixed bugs

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3091;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
